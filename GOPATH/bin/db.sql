e46d54a646c935977d1b4dfa871922b160370e83
timeloveboy <734991033@qq.com>
2018-12-19 17:27:19 +0800

    db.sql

---
+++ GOPATH/bin/db.sql
@@ -0,0 +1,30 @@
+create schema lightpan;
+CREATE TABLE `groupuser` (
+  `id` varchar(45) NOT NULL,
+  `user` varchar(45) NOT NULL,
+  `jointime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE KEY `groupuser` (`id`,`user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `id` varchar(45) NOT NULL,
+  `name` varchar(100) NOT NULL DEFAULT '用户',
+  `password` varchar(256) NOT NULL DEFAULT '' COMMENT ' ',
+  `face` varchar(256) DEFAULT NULL,
+  `registetime` datetime NOT NULL,
+  `type` varchar(45) NOT NULL DEFAULT '',
+  `parent` varchar(45) NOT NULL DEFAULT '' COMMENT 'if type=group,parent = group owner',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `file` (
+  `user` varchar(45) NOT NULL,
+  `folder` varchar(200) DEFAULT NULL,
+  `name` varchar(260) NOT NULL,
+  `createtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `version` int(11) NOT NULL DEFAULT '0',
+  `object4d` varchar(260) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
