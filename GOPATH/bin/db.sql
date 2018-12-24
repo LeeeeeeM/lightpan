4a76d7dde81f04df8aee172814795f982a4cdbbc
timeloveboy <734991033@qq.com>
2018-12-24 23:57:23 +0800

    f,m service

--- GOPATH/bin/db.sql
+++ GOPATH/bin/db.sql
@@ -23,9 +23,12 @@ CREATE TABLE `file` (
   `user` varchar(45) NOT NULL,
   `folder` varchar(200) DEFAULT NULL,
   `name` varchar(260) NOT NULL,
+  `pub` tinyint(1) NOT NULL DEFAULT '1',
+  `del` tinyint(1) NOT NULL DEFAULT '1',
   `createtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `version` int(11) NOT NULL DEFAULT '0',
-  `object4d` varchar(260) NOT NULL
+  `object4d` varchar(260) NOT NULL,
+  UNIQUE KEY `furl` (`user`,`folder`,`name`,`del`,`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
