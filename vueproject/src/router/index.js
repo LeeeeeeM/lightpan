70926df2f697117fc6276c913100ab98ce67aec5
timeloveboy <734991033@qq.com>
2018-12-26 22:05:58 +0800

    最重要的3个页面

--- vueproject/src/router/index.js
+++ vueproject/src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import folder from '@/page/folder'
+import login from '@/page/login'
+import upload from '@/page/upload'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: login
+    },{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+      name: 'folder',
+      component: folder
+    },{
+      path: '/upload',
+      name: 'upload',
+      component: upload
+    },
+  ],
+  mode:'history'
 })
